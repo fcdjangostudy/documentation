8c0b538eec401438ab3117e2956da2bb587be37e
GAIUS8 <gaius827@gmail.com>
2017-06-05 12:51:55 +0900

    modified Pseson models(choices, recursive foreignkey, Car, Manufaturer)

--- django_app/introduction_to_models/admin.py
+++ django_app/introduction_to_models/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Person)
+admin.site.register(Manufacturer)
+admin.site.register(Car)
--- django_app/introduction_to_models/models.py
+++ django_app/introduction_to_models/models.py
@@ -7,5 +7,47 @@ class Person(models.Model):
         ('M', 'medium'),
         ('L', 'large'),
     )
-    name = models.CharField(max_length=60)
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    name = models.CharField('이름', max_length=60)
+    shirt_size = models.CharField(
+        '셔츠사이즈',
+        max_length=1,
+        choices=SHIRT_SIZES,
+        help_text='Man is L actually'
+    )
+    TYPES = (
+        ('student', '학생'),
+        ('tescher', '선생'),
+    )
+    pserson_type = models.CharField(
+        '유형',
+        max_length=10,
+        choices=TYPES,
+        default=TYPES[0][0],
+    )
+    teacher = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=40)
+    manufaturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
